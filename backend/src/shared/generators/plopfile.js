d27bfadc814b5aa9ee13cd30ad3eb300597a5ced
Felipe Borges <lipex360@gmail.com>
2020-12-09 17:54:14 -0300

    Plop updated

--- backend/src/shared/generators/plopfile.js
+++ backend/src/shared/generators/plopfile.js
@@ -8,28 +8,59 @@ module.exports = function (plop) {
       message: 'Module name please'
     }],
     actions: [
+
+      // INFRA: http
       {
         type: 'add',
-        path: '../../modules/{{camelCase name}}/.gitkeep'
-      },
-      {
-        type: 'add',
-        path: '../../modules/{{camelCase name}}/repositories/.gitkeep'
-      },
-      {
-        type: 'add',
-        path: '../../modules/{{camelCase name}}/services/.gitkeep'
-      },
-      {
-        type: 'add',
-        path: '../../modules/{{camelCase name}}/infra/http/controllers/.gitkeep'
+        path: '../../modules/{{camelCase name}}/infra/http/controllers/Create{{pascalCase name}}Controller.ts',
+        templateFile: 'templates/controller.hbs'
       },
       {
         type: 'add',
         path: '../../modules/{{camelCase name}}/infra/http/routes/{{camelCase name}}.routes.ts',
-        templateFile: 'templates/index.ts.hbs'
-      }
+        templateFile: 'templates/routes.hbs'
+      },
 
+      // INFRA: typeORM
+      {
+        type: 'add',
+        path: '../../modules/{{camelCase name}}/infra/typeorm/entities/{{pascalCase name}}.ts',
+        templateFile: 'templates/entities.hbs'
+      },
+      {
+        type: 'add',
+        path: '../../modules/{{camelCase name}}/infra/typeorm/repositories/{{pascalCase name}}Repository.ts',
+        templateFile: 'templates/repository.hbs'
+      },
+
+      // REPOSITORIES
+      {
+        type: 'add',
+        path: '../../modules/{{camelCase name}}/repositories/index.ts',
+        templateFile: 'templates/indexContainer.hbs'
+      },
+      {
+        type: 'add',
+        path: '../../modules/{{camelCase name}}/repositories/fakes/Fake{{pascalCase name}}Repository.ts',
+        templateFile: 'templates/fakeRepository.hbs'
+      },
+      {
+        type: 'add',
+        path: '../../modules/{{camelCase name}}/repositories/interfaces/I{{pascalCase name}}Repository.ts',
+        templateFile: 'templates/interfaceRepository.hbs'
+      },
+
+      // SERVICES
+      {
+        type: 'add',
+        path: '../../modules/{{camelCase name}}/services/Create{{pascalCase name}}/Create{{pascalCase name}}Service.ts',
+        templateFile: 'templates/service.hbs'
+      },
+      {
+        type: 'add',
+        path: '../../modules/{{camelCase name}}/services/Create{{pascalCase name}}/Create{{pascalCase name}}Service.spec.ts',
+        templateFile: 'templates/service.spec.hbs'
+      }
     ]
   })
 }
